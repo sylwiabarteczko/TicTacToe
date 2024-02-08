94f30fb751a27141e790727ee599534bbc6c5a36
Roger Zacharczyk <roger.zacharczyk@gmail.com>
2024-02-08 10:03:49 +0100

    Wymagania do CharacterPoolRandomizer

---
+++ src/main/java/syll25/tictactoe/CharacterPoolRandomizer.java
@@ -0,0 +1,11 @@
+package syll25.tictactoe;
+
+/**
+ * Wymagania:
+ * - konstruktor przyjmujący zestaw znaków, z których będziemy losować
+ * - metoda losująca znak z puli, bez powtórzeń
+ * - metoda losująca w przypadku braku dostępnych w puli znaków rzuca wyjątek
+ * - testy jednostkowe
+ */
+public class CharacterPoolRandomizer {
+}
