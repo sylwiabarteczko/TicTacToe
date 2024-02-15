eecf42b6401df3157160e456ed1112026b699168
Sylwia B <sylwia.barteczko95@gmail.com>
2024-02-15 14:05:40 -0800

    Most of the changes submit after lesson 15.02

--- src/main/java/syll25/tictactoe/CharacterPoolRandomizer.java
+++ src/main/java/syll25/tictactoe/CharacterPoolRandomizer.java
@@ -1,5 +1,10 @@
 package syll25.tictactoe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Wymagania:
  * - konstruktor przyjmujący zestaw znaków, z których będziemy losować
@@ -8,4 +13,42 @@ package syll25.tictactoe;
  * - testy jednostkowe
  */
 public class CharacterPoolRandomizer {
+    private ArrayList<Character> availableSymbols;
+    private List<Character> symbols; // Bez tego podkresla na czerwono kazde slowo "symbols" z komentarzem "cannot resolve symbol"
+
+    public CharacterPoolRandomizer(char... characters) {
+        symbols = new ArrayList<>();
+        for (char c : characters) {
+            symbols.add(c);
+        }
+
+        if (symbols.size() != 5) {
+            throw new IllegalArgumentException("You need to choose one of this characters: X, Y, Z, O, S"); //nazwe wyjatku podpowiada mi program, gdy wymysle wlasna stwierdza "cannot resolve symbol"
+        }
+        availableSymbols = new ArrayList<>(symbols);
+        Collections.shuffle(availableSymbols);
+    }
+
+    public void addSymbols(char... characters) {
+        for (char symbol : symbols) {
+            availableSymbols.add(symbol);
+        }
+    }
+
+    public char drawSymbol() {
+        if (availableSymbols.isEmpty()) {
+            throw new IllegalArgumentException("No available symbols");
+        }
+        int randomIndex = new Random().nextInt(availableSymbols.size());
+        char symbol = availableSymbols.remove(0);
+        return symbol;
+    }
+
+    public List<Character> drawSymbols() {
+        List<Character> drawnSymbols = new ArrayList<>();
+        for (char symbol : symbols) {
+            drawnSymbols.add(symbol);
+        }
+        return drawnSymbols;
+    }
 }
--- src/main/java/syll25/tictactoe/Main.java
+++ src/main/java/syll25/tictactoe/Main.java
@@ -9,21 +9,15 @@ public class Main {
 //        Player player1 = new Player();
 //        Player player2 = new Player();
 
-        TicTacToe symbolChoice = new TicTacToe('X', 'Y', 'Z', 'O', 'S');
+        CharacterPoolRandomizer symbolChoice = new CharacterPoolRandomizer('X', 'Y', 'Z', 'O', 'S');
 
         List<Character> drawSymbols = symbolChoice.drawSymbols();
 
-        char player1symbol = drawSymbols.get(0);
+        char player1symbol = symbolChoice.drawSymbol();
         System.out.println("Player 1 that is your symbol: " + player1symbol);
 
-        char player2symbol = drawSymbols.get(1);
+        char player2symbol = symbolChoice.drawSymbol();
         System.out.println("Player 2 that is your symbol: " + player2symbol);
-
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Hello Player 1, what is your name? ");
-//        String play1 = scanner.nextLine();
-//        System.out.println("Hello Player 2, what is your name? ");
-//        String play2 = scanner.nextLine();
 
 
 //        char [][] board = new char[2][2];
