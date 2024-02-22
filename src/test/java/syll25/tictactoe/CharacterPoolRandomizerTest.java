3da56fdf09bb54f6720a56de4900161b87430893
Sylwia B <sylwia.barteczko95@gmail.com>
2024-02-21 17:08:16 -0800

    New test return single symbol, no list of symbols. Also checking if pool is not empty.

--- src/test/java/syll25/tictactoe/CharacterPoolRandomizerTest.java
+++ src/test/java/syll25/tictactoe/CharacterPoolRandomizerTest.java
@@ -1,34 +1,26 @@
 package syll25.tictactoe;
 
 import org.junit.jupiter.api.Test;
-import java.util.List;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class CharacterPoolRandomizerTest {
 
     @Test
     public void testDrawSymbol() {
 
-
         //given
         CharacterPoolRandomizer symbolChoice = new CharacterPoolRandomizer('X', 'Y', 'Z', 'O', 'S');
+        Set<Character> availableSymbols = new HashSet<>(Arrays.asList('X', 'Y', 'Z', 'O', 'S'));
+
         //when
-        List<Character> characters = symbolChoice.drawSymbols();
+        char drawSymbol = symbolChoice.drawSymbol();
 
         //then
-        assertNotNull(characters);
-
-        assertEquals(5, characters.size());
-
-        assertTrue(characters.contains('X'));
-
-        assertTrue(characters.contains('Y'));
-
-        assertTrue(characters.contains('Z'));
-
-        assertTrue(characters.contains('O'));
-
-        assertTrue(characters.contains('S'));
-
-
+        assertTrue(drawSymbol == 'X' || drawSymbol == 'Y' || drawSymbol == 'Z' || drawSymbol == 'O' || drawSymbol == 'S');
+        assertTrue(availableSymbols.contains(drawSymbol), "Symbol is not available");
     }
 }
